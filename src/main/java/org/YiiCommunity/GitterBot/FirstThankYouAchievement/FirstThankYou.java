87d18aecd44a4a3edfe8cb25f19bb2266a114246
Alex Bond <alex.bondarevskiy@gmail.com>
2015-11-01 03:46:12 -0800

    Auto adding to db on first start.

--- src/main/java/org/YiiCommunity/GitterBot/FirstThankYouAchievement/FirstThankYou.java
+++ src/main/java/org/YiiCommunity/GitterBot/FirstThankYouAchievement/FirstThankYou.java
@@ -1,5 +1,6 @@
 package org.YiiCommunity.GitterBot.FirstThankYouAchievement;
 
+import com.avaje.ebean.Ebean;
 import org.YiiCommunity.GitterBot.containers.Gitter;
 import org.YiiCommunity.GitterBot.models.database.Achievement;
 import org.YiiCommunity.GitterBot.models.database.User;
@@ -9,6 +10,8 @@ public class FirstThankYou extends org.YiiCommunity.GitterBot.api.Achievement {
     public FirstThankYou() {
         setCodeName("firstThankYou");
         setType(TYPE.ONE_TIME);
+
+        checkAndInstall();
     }
 
     public void onUserChange(User user) {
@@ -18,6 +21,19 @@ public class FirstThankYou extends org.YiiCommunity.GitterBot.api.Achievement {
             Gitter.sendMessage(obj.getChatAnnounce().replace("{username}", user.getUsername()));
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private void checkAndInstall() {
+        try {
+            Achievement.getAchievement(getCodeName());
+        } catch (Exception e) {
+            Achievement obj = new Achievement();
+            obj.setKey(this.getCodeName());
+            obj.setChatAnnounce("First thanks to @{username}!");
+            obj.setTitle("First thanks in chat");
+            obj.setDescription("First thanks in chat");
+            Ebean.save(obj);
         }
     }
 }
